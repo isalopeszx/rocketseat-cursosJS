9456d628f28d2c6061b15724b3a620fbb5ae3d0e
Isabela Lopes <isabela_m_silva@estudante.sc.senai.br>
2019-06-26 16:27:22 -0300

    Começando aula 3, mod 3

---
+++ mod3/aula/4/todo.js
@@ -0,0 +1,36 @@
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+
+var buttonElement = document.querySelector("#app button")
+
+var todos = [
+    'fazer café',
+    'beber água',
+    'reciclar'
+
+];
+
+function renderTodos() {
+    for (todo of todos) {
+
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+
+}
+
+renderTodos();
+
+function addTodo (){
+var todoText = inputElement.Value;
+
+todos.push(todoText);
+inputElement,value = "";
+renderTodos();
+
+}
+
+buttonElement.onclick = addTodo;
